481dc4ec6499836ff8a059dec435f69034f200bb
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-01-13 13:49:29 +0300

    Make Assertion With SoftAssert && assertAll

--- TestNGWithSelenium/src/jafarloka/CheckAssertion.java
+++ TestNGWithSelenium/src/jafarloka/CheckAssertion.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class CheckAssertion {
 
@@ -109,5 +110,59 @@ public class CheckAssertion {
 		WebElement t1 = driver.findElement(By.xpath("//nav[@class='oxd-navbar-nav']"));
 		
 		Assert.assertTrue(t1.isDisplayed());
+	}
+
+	@Test
+	public void checkAssertionsWithSoftAssert() {
+		
+		System.setProperty(
+			"webdriver.chrome.driver", 
+			"C:\\Tests\\Python-Selenium-From-Tutorial-01\\chromedriver.exe"
+		);
+			
+		ChromeDriver driver = new ChromeDriver();
+			
+		driver.get("https://opensource-demo.orangehrmlive.com");
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		
+		WebElement el1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
+		
+		el1.sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		
+		driver.findElement(By.xpath("//button[text()[contains(., 'Login')]]")).click();
+		
+		WebElement t1 = wait.until(
+			ExpectedConditions.presenceOfElementLocated(
+				By.xpath("//nav[@class='oxd-navbar-nav']")
+			)
+		);
+		
+//		WebElement t1 = driver.findElement(By.xpath("//nav[@class='oxd-navbar-nav']"));
+		
+		SoftAssert softAssert = new SoftAssert();
+		
+//		softAssert.assertTrue(!t1.isDisplayed());
+		
+		softAssert.assertTrue(t1.isDisplayed());
+		
+		WebElement t2 = driver.findElement(By.xpath("//button[@class='oxd-icon-button']"));
+		
+		softAssert.assertTrue(t2.isEnabled());
+		
+		t2.click();
+		
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		driver.quit();
+		
+		softAssert.assertAll();		
+		
 	}
 }
