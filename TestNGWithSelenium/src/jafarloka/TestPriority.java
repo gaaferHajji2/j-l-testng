bfc139a2f64e5e3d0dd652c6a0bb5ea7de79a82c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-22 12:23:11 +0300

    Initial Example About Priority In TestNG

---
+++ TestNGWithSelenium/src/jafarloka/TestPriority.java
@@ -0,0 +1,26 @@
+package jafarloka;
+
+import org.testng.annotations.Test;
+
+public class TestPriority {
+
+	@Test
+	public void a() {
+		System.out.println("This Is The Test Case a");
+	}
+	
+	@Test
+	public void c() {
+		System.out.println("This Is The Test Case c");
+	}
+	
+	@Test
+	public void b() {
+		System.out.println("This Is The Test Case b");
+	}
+	
+	@Test
+	public void d() {
+		System.out.println("This Is The Test Case d");
+	}
+}
