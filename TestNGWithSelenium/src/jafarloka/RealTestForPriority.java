5b908927f32681c00975124762a91764ef4205be
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-22 13:18:05 +0300

    Finish From Real Test For Priority

--- TestNGWithSelenium/src/jafarloka/RealTestForPriority.java
+++ TestNGWithSelenium/src/jafarloka/RealTestForPriority.java
@@ -9,23 +9,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RealTestForPriority {
+	
+	ChromeDriver driver;
+	
+	ChromeDriverService service;
+	
+	File serviceFile;
+	
+	WebDriverWait wait;
+	
+	String homePageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
 
 	@Test(priority = 1)
 	public void testLogin() {
-		File serviceFile = new File("C:\\Tests\\Python-Selenium-From-Tutorial-01\\chromedriver.exe");
-		
-		ChromeDriverService service;
+		serviceFile = new File("C:\\Tests\\Python-Selenium-From-Tutorial-01\\chromedriver.exe");
 		
 		service = new ChromeDriverService.Builder().usingDriverExecutable(serviceFile).usingAnyFreePort().build();
 		
-		ChromeDriver driver = new ChromeDriver(service);
+		driver = new ChromeDriver(service);
 		
 		driver.get("https://opensource-demo.orangehrmlive.com");
 		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		
 		WebElement el1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
 		
@@ -33,24 +42,28 @@ public class RealTestForPriority {
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		
 		driver.findElement(By.xpath("//button[text()[contains(., 'Login')]]")).click();
+		
+		Assert.assertEquals(driver.getCurrentUrl(), homePageUrl);
 	}
 	
 	// Leave XPATH Is --> //a/span[text()='Leave']
+	// For Employee Name --> //input[@placeholder='Type for hints...']
 	@Test(priority = 2)
 	public void testCheckLeave() {
-		
-		File serviceFile = new File("C:\\Tests\\Python-Selenium-From-Tutorial-01\\chromedriver.exe");
-		
-		ChromeDriverService service;
-		
-		service = new ChromeDriverService.Builder().usingDriverExecutable(serviceFile).usingAnyFreePort().build();
-		
-		ChromeDriver driver = new ChromeDriver(service);
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		
 		WebElement el1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a/span[text()='Leave']")));
 		
 		el1.click();
+		
+		WebElement el2 = wait.until(
+				ExpectedConditions.presenceOfElementLocated(
+							By.xpath("//input[@placeholder='Type for hints...']")
+				)
+			);
+		
+		el2.sendKeys("John");
+		
+		Assert.assertTrue(el2.isDisplayed());
 	}
 	
 	// XPATH Of Username --> //p[@class='oxd-userdropdown-name']
@@ -58,10 +71,26 @@ public class RealTestForPriority {
 	@Test(priority = 3)
 	public void testLogout() {
 		
+		WebElement userNameElement = driver.findElement(By.xpath("//p[@class='oxd-userdropdown-name']"));
+		
+		userNameElement.click();
+		
+		WebElement logoutElement = wait.until(
+				ExpectedConditions.presenceOfElementLocated(
+						By.linkText("Logout")
+				)
+		);
+		
+		logoutElement.click();
+		
+		Assert.assertNotEquals(driver.getCurrentUrl(), homePageUrl);
+		
 	}
 	
 	@Test(priority = 4)
 	public void exitFromBrowser() {
+		
+		driver.quit();
 		
 	}
 	
