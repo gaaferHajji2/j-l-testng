0d28a682527ca76960fa89429997c222a539e379
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-22 12:52:08 +0300

    Building Tests For Login And Leave Label

--- TestNGWithSelenium/src/jafarloka/RealTestForPriority.java
+++ TestNGWithSelenium/src/jafarloka/RealTestForPriority.java
@@ -9,10 +9,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 public class RealTestForPriority {
 
-	@Test
+	@Test(priority = 1)
 	public void testLogin() {
 		File serviceFile = new File("C:\\Tests\\Python-Selenium-From-Tutorial-01\\chromedriver.exe");
 		
@@ -32,6 +33,36 @@ public class RealTestForPriority {
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		
 		driver.findElement(By.xpath("//button[text()[contains(., 'Login')]]")).click();
+	}
+	
+	// Leave XPATH Is --> //a/span[text()='Leave']
+	@Test(priority = 2)
+	public void testCheckLeave() {
+		
+		File serviceFile = new File("C:\\Tests\\Python-Selenium-From-Tutorial-01\\chromedriver.exe");
+		
+		ChromeDriverService service;
+		
+		service = new ChromeDriverService.Builder().usingDriverExecutable(serviceFile).usingAnyFreePort().build();
+		
+		ChromeDriver driver = new ChromeDriver(service);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		
+		WebElement el1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a/span[text()='Leave']")));
+		
+		el1.click();
+	}
+	
+	// XPATH Of Username --> //p[@class='oxd-userdropdown-name']
+	// By Link Txt Is --> Logout
+	@Test(priority = 3)
+	public void testLogout() {
+		
+	}
+	
+	@Test(priority = 4)
+	public void exitFromBrowser() {
+		
 	}
 	
 }
