4253cae430e5b41195b060ed671080dc7d6cdeb7
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-28 15:05:41 +0300

    adding optional parameter

--- TestNGWithSelenium/src/jafarloka/TestParametersWithTestNG.java
+++ TestNGWithSelenium/src/jafarloka/TestParametersWithTestNG.java
@@ -1,16 +1,19 @@
 package jafarloka;
 
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestParametersWithTestNG {
 
-	@Parameters({"jloka01", "jloka02"})
+	@Parameters({"jloka01", "jloka02", "jloka03"})
 	@Test
-	public void testParameters(String jloka01, String jloka02) {
+	public void testParameters(String jloka01, String jloka02, @Optional("Jafar-Loka-03") String jloka03) {
 		System.out.println("The jloka01 is: " + jloka01);
 		
 		System.out.println("The jloka02 is: " + jloka02);
+		
+		System.out.println("The jloka03 is: " + jloka03);
 	}
 	
 }
