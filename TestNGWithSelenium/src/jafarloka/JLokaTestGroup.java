efdf8a253f92c00f254b8ebe30c5e53e6fba6472
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-05 13:59:54 +0300

    Adding groups test

---
+++ TestNGWithSelenium/src/jafarloka/JLokaTestGroup.java
@@ -0,0 +1,32 @@
+package jafarloka;
+
+import org.testng.annotations.Test;
+
+public class JLokaTestGroup {
+
+	@Test
+	public void jLoka01() {
+		System.out.println("Call From JLoka-01");
+	}
+	
+	@Test
+	public void jLoka02() {
+		System.out.println("Call From JLoka-02");
+	}
+	
+	@Test
+	public void jLoka03() {
+		System.out.println("Call From JLoka-03");
+	}
+	
+	@Test
+	public void jLoka04() {
+		System.out.println("Call From JLoka-04");
+	}
+	
+	@Test
+	public void jLoka05() {
+		System.out.println("Call From JLoka-05");
+	}
+	
+}
