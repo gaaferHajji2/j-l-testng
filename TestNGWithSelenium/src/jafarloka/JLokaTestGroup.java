033a7af9d1001029c01f60fe41451b7a173fb300
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-05 14:21:45 +0300

    Build groups for testing

--- TestNGWithSelenium/src/jafarloka/JLokaTestGroup.java
+++ TestNGWithSelenium/src/jafarloka/JLokaTestGroup.java
@@ -4,27 +4,27 @@ import org.testng.annotations.Test;
 
 public class JLokaTestGroup {
 
-	@Test
+	@Test(groups = "test-01")
 	public void jLoka01() {
 		System.out.println("Call From JLoka-01");
 	}
 	
-	@Test
+	@Test(groups = "test-01")
 	public void jLoka02() {
 		System.out.println("Call From JLoka-02");
 	}
 	
-	@Test
+	@Test(groups = "test-02")
 	public void jLoka03() {
 		System.out.println("Call From JLoka-03");
 	}
 	
-	@Test
+	@Test(groups = "test-02")
 	public void jLoka04() {
 		System.out.println("Call From JLoka-04");
 	}
 	
-	@Test
+	@Test(groups = "test-03")
 	public void jLoka05() {
 		System.out.println("Call From JLoka-05");
 	}
