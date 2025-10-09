cf6e76ec9d0142e8205c7867827f6f9dc498b627
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-09 15:20:09 +0300

    Testing depends on groups

--- TestNGWithSelenium/src/jafarloka/JLokaTestDependsOn.java
+++ TestNGWithSelenium/src/jafarloka/JLokaTestDependsOn.java
@@ -3,7 +3,7 @@ package jafarloka;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class JLokaTestDependsOnMethods {
+public class JLokaTestDependsOn {
 
 	@Test(dependsOnMethods = {"test2"})
 	public void test1() {
@@ -11,10 +11,19 @@ public class JLokaTestDependsOnMethods {
 		
 	}
 	
-	@Test
+	@Test(dependsOnGroups = {"t-01"})
 	public void test2() {
 		System.out.println("JLoka-01 Test-01");
 		Assert.assertEquals(true, true);
 	}
 	
+	@Test(groups = "t-01")
+	public void test3() {
+		System.out.println("JLoka-01 Test-03 group-01");
+	}
+	
+	@Test(groups= "t-01")
+	public void test4() {
+		System.out.println("JLoka-01 Test-03 group-02");
+	}
 }
